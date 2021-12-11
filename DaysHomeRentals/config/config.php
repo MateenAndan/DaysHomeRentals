8c85a26c75bb94bf3ac2ff8ed07e1849fe5a2e2c
MateenAndan <77847288+MateenAndan@users.noreply.github.com>
2021-12-11 14:23:19 +0000

    Update config.php

--- DaysHomeRentals/config/config.php
+++ DaysHomeRentals/config/config.php
@@ -1,6 +1,6 @@
 <?php 
 
-$db = new mysqli('localhost','root','admin','homerents');
+$db = new mysqli('localhost','root','admin','homerents',20.108.172.137);
 
 if($db->connect_error){
 	echo "Error connecting database";
