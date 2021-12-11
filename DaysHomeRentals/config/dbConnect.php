f8712b77a860a037126adddbd215cd255ef53bf1
MateenAndan <77847288+MateenAndan@users.noreply.github.com>
2021-12-11 04:28:49 +0000

    Update dbConnect.php

--- DaysHomeRentals/config/dbConnect.php
+++ DaysHomeRentals/config/dbConnect.php
@@ -1,6 +1,6 @@
 <?php 
 require __DIR__ . "/dbCredentials.php";
-$db = new mysqli(servername, username, password, dbname, 3307);
+$db = new mysqli(servername, username, password, dbname);
 if($db->connect_error){
 	echo "Error connecting database";
 }
